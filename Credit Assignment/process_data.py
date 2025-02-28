518fff013113197caba84aee4c36dac2c54b723f
thuanbui1309 <buiminhthuan1314@gmail.com>
2025-02-28 10:30:38 +0700

    Preprocess data for credit assignment

---
+++ Credit Assignment/process_data.py
@@ -0,0 +1,10 @@
+# The intial data is not in correct structure for model training, so we need to transform it to the correct format
+# Originaly, here is the format of each image:
+
+#     image’s name{space}class label
+
+
+DATA_TRAIN_PATH = "data/Train"
+ANNOTATION_TRAIN_TRAIN = "data/Train/train.txt"
+DATA_TEST_PATH = "data/Test"
+ANNOTATION_TEST_TRAIN = "data/Train/test.txt"
